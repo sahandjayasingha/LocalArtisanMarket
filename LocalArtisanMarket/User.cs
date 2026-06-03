071ef21fb2c2b5747aa59aab85e696be64d8ab85
user <user@DESKTOP-0KOC4IV>
2026-06-03 22:13:17 +0530

    Add project files.

---
+++ LocalArtisanMarket/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LocalArtisanMarket
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+
+    public class Artisan : User
+    {
+        public string VillageLocation { get; set; }
+        public string CraftSpecialization { get; set; }
+    }
+
+    public class Customer : User
+    {
+        public string DeliveryAddress { get; set; }
+    }
+}
