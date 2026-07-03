9522bb593da003ea674935e319d993f8e097fba7
Sandith-alv <alwissandith@gmail.com>
2026-07-03 09:19:25 +0530

    customer dashboard

---
+++ LocalArtisanMarket/MarketModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LocalArtisanMarket
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockAvailable { get; set; }
+    }
+
+    public class CartItem
+    {
+        public Product SelectedProduct { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => SelectedProduct.Price * Quantity;
+    }
+}
